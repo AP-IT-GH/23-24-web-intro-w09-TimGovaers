cf040d18133f37c7b3c67313eb7eacbb473d6e0a
TimGovaers <tim.govaers1208@gmail.com>
2024-04-23 12:52:39 +0200

    map werkt.

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -2,7 +2,13 @@
 * Gebruik hiervoor de documentatie op https://leafletjs.com/ 
 * Verander in de code online alle "var" in "let".
 */
+let map = L.map('apMap').setView([51.23009, 4.41616],17);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom:19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+script
 
 
 
@@ -14,7 +20,7 @@
 
 
 // bepaal de rechthoek rondom het gebouw van AP
-
+let bounds = [[51.23041,4.4155][51.22991,4.1675]];
 // kleur de rechthoek in met de rode AP-kleur
-
+L.rectangle(bounds, {color:"e60005"})
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
